b493926ea0f9f575c348bbd46b893cbbf84ef050
chaknogf <chaknogf@gmail.com>
2023-12-28 18:36:48 -0600

    rendimiento

--- routers/consultas_router.py
+++ routers/consultas_router.py
@@ -8,8 +8,9 @@ from models.consulta import ConsultasModel, VistaConsultas
 from models.paciente import PacienteModel
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-from sqlalchemy import func, select
+from sqlalchemy import func, select, desc
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import lazyload
 
 router = APIRouter()
 db = database.get_database_connection()
@@ -58,13 +59,22 @@ class Consultas(BaseModel):
 @router.get("/consultas/", tags=["Consultas"])
 async def obtener_consultas():
     try:
-        db = Session()
-        result = db.query(ConsultasModel).all()
-        return JSONResponse(status_code=200, content=jsonable_encoder(result))
+        with Session() as db:
+            result = (
+                db.query(ConsultasModel)
+                .order_by(desc(ConsultasModel.id))  # Ordena por id en orden descendente
+                .limit(10000)  # Ajusta el límite según tus necesidades
+                .options(lazyload('*'))  # Si es necesario cargar relaciones de manera diferida
+                .all()
+            )
+            if not result:
+                return JSONResponse(status_code=200, content=jsonable_encoder([]))
+
+            return result
+
     except SQLAlchemyError as error:
-        return {"message": f"error al consultar: {error}"}
-    finally:
-        print("consultado")
+        raise HTTPException(status_code=500, detail=f"Error al consultar: {error}")
+    
    
 ConsultasModel = ConsultasModel
 @router.get("/filtro/", tags=["Consultas"])
@@ -169,25 +179,27 @@ async def consultas(fecha: str, tipo: int):
     try: 
         db = Session()
         NoEncontrado = []
-        
         result = (
-            db.query(ConsultasModel)
+            db.query(ConsultasModel, PacienteModel.nombre, PacienteModel.apellido)
+            .join(ConsultasModel.pacientes)
             .filter(ConsultasModel.fecha_consulta == fecha, ConsultasModel.tipo_consulta == tipo)
         ).all()
         
         if not result:
             return JSONResponse(status_code=200, content=jsonable_encoder(NoEncontrado))
 
-        consulta = result[0]  # Desempaquetar la tupla
+        consulta, name, lastname = result[0]  # Desempaquetar la tupla
+        
         consulta_dict = consulta.__dict__
-
+        consulta_dict["name"] = name
+        consulta_dict["lastname"] = lastname
+        
         print(f"expediente: {consulta.id} datetime: {now} CONSULTADO")
         return JSONResponse(status_code=200, content=jsonable_encoder(consulta_dict))
     except SQLAlchemyError as error:
         raise HTTPException(status_code=500, detail=f"Error al consultar: {error}")
     finally:
         db.close()
-
 
         
 @router.get("/consultando/", tags=["Consultas"])
@@ -330,7 +342,11 @@ async def consult(tipo: int):
         result = (
             db.query(ConsultasModel)
             .filter(ConsultasModel.tipo_consulta == tipo)
-        ).all()
+                .order_by(desc(ConsultasModel.id))  # Ordena por id en orden descendente
+                .limit(10000)  # Ajusta el límite según tus necesidades
+                .options(lazyload('*'))  # Si es necesario cargar relaciones de manera diferida
+                .all()
+        )
         
         if not result:
             return JSONResponse(status_code=200, content=jsonable_encoder(NoEncontrado))
@@ -347,6 +363,28 @@ async def consult(tipo: int):
         raise HTTPException(status_code=500, detail=f"Error al consultar: {error}")
     finally:
         db.close()
+
+@router.get("/emerg/", tags=["Consultas"])
+async def consult_3():
+    try:
+        with Session() as db:
+            result = (
+                db.query(ConsultasModel)
+                .filter(ConsultasModel.tipo_consulta == 3)
+                .order_by(desc(ConsultasModel.id))  # Ordena por id en orden descendente
+                .limit(10000)  # Ajusta el límite según tus necesidades
+                .options(lazyload('*'))  # Si es necesario cargar relaciones de manera diferida
+                .all()
+            )
+
+            if not result:
+                return JSONResponse(status_code=200, content=jsonable_encoder([]))
+
+            return result
+
+    except SQLAlchemyError as error:
+        raise HTTPException(status_code=500, detail=f"Error al consultar: {error}")
+
                 
  
 #       #Post conectado a SQL
@@ -477,7 +515,6 @@ async def eliminar_consulta(id: int):
         return {"message": f"Error al consultar: {error}"}
     finally:
             print(f" ELIMINADO realizado")
-
 
 
 
